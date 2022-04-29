e44efd40fc42679a85c2c7d0d4c82f2a6c425fbb
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-04-29 11:09:55 +0530

    1.0.0 --> ConnectMe app build successfully and connected to firebase realtime database

---
+++ app/src/main/java/com/example/connectme/users/Registration.kt
@@ -0,0 +1,12 @@
+package com.example.connectme.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.connectme.R
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
