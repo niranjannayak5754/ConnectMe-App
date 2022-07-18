8f0a2835ec926442912f3847006668a2a2957cb3
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-07-18 16:42:39 +0530

    2.0.2 ---> deleted files

--- app/src/main/java/com/example/connectme/utils/model/BuyPet.kt
+++
@@ -1,7 +0,0 @@
-package com.example.connectme.utils.model
-
-
-data class BuyPet(
-    val quantity: Int = 0,
-    val totalPetPrice: Double = 0.0
-)
---
+++ app/src/main/java/com/example/connectme/utils/model/CartItem.kt
@@ -0,0 +1,8 @@
+package com.example.connectme.utils.model
+
+
+data class BuyPet(
+    var quantity: String = "0",
+    var totalPetPrice: String = "0.0",
+    var petId: String? = null
+)
